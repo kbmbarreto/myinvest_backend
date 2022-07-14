01815ddf5868783d752ca17372e4ae133c5575cf
cskmacedo <kleber.barreto@msn.com>
2022-07-14 02:18:20 -0300

    Refatoração

--- src/main/kotlin/br/com/lambdateam/myinvest/model/response/PostUser.kt
+++
@@ -1,8 +0,0 @@
-package br.com.lambdateam.myinvest.model.response
-
-data class PostUser (
-
-    val userName: String,
-    val email: String,
-    val password: String
-)
--- src/main/kotlin/br/com/lambdateam/myinvest/model/response/PutUser.kt
+++
@@ -1,9 +0,0 @@
-package br.com.lambdateam.myinvest.model.response
-
-data class PutUser (
-
-    val id: Long,
-    val userName: String,
-    val email: String,
-    val password: String
-)
