18d2de71b2434069c64b3cb8f3c4f2cfc7652f22
cskmacedo <kleber.barreto@msn.com>
2022-07-13 23:03:32 -0300

    Adicionado id_user em todas as models

--- src/main/kotlin/br/com/lambdateam/myinvest/model/UserModel.kt
+++ src/main/kotlin/br/com/lambdateam/myinvest/model/UserModel.kt
@@ -9,10 +9,13 @@ data class UserModel(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+
     @Column(name = "username", length = 60)
     var userName: String,
+
     @Column(name = "email", length = 75)
     var email: String,
+
     @Column(name = "password", length = 256)
     var password: String
 )
